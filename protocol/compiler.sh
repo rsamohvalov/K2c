9c128ad915ab83b834ebef0feacab081b8240726
R.Samokhvalov <r.samokhvalov@kryptonite.ru>
2023-01-18 17:02:23 +0300

    2 services K2c

--- protocol/compiler.sh
+++ protocol/compiler.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/projects/gRPC/install/bin/protoc --proto_path=protos --cpp_out=classes/cpp protos/K2c.proto
+/projects/gRPC/install/bin/protoc --proto_path=protos --cpp_out=classes/cpp protos/K2cMCC.proto
+/projects/gRPC/install/bin/protoc --proto_path=protos --cpp_out=classes/cpp protos/K2cFSP.proto
